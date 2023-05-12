8aab1598a56752fa2fcae5ae02dee05d85c8b77a
Alex <alexgjohn@outlook.com>
2023-05-12 19:03:58 +0100

    cracked isogram mvp, but could be more accurate

--- hw_advanced_start/isogram_finder/isogram_finder.js
+++ hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,17 @@
 const IsogramFinder = function (word) {
-
+    this.word = word
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+    const wordAsArray = this.word.split('')
 
+    const wordWithoutDuplicates = wordAsArray.filter((letter, index, list) => {
+        return index === list.indexOf(letter)
+    })
+
+    const isogram = wordWithoutDuplicates.length === wordAsArray.length
+
+    return isogram
 }
 
 module.exports = IsogramFinder;
--- hw_advanced_start/pangram_finder/pangram_finder.js
+++ hw_advanced_start/pangram_finder/pangram_finder.js
@@ -15,22 +15,11 @@ PangramFinder.prototype.isPangram = function () {
   const pangram = this.alphabet.every((letter) => {
     return phraseInLower.includes(letter)
   })
-  // const pangram = phraseWithoutSpaces.every((letter) => {
-  //   return this.alphabet.includes(letter)
-  // })
+
   return pangram
 }
 
-// Cinema.prototype.areAllFilmsOfMinLength = function (length) {
-//   return this.films.every((film) => {
-//     return film.length >= length;
-//   });
-// };
+
 
 module.exports = PangramFinder;
 
-// const pangramFinder = new PangramFinder('the quick brown fox jumps over the lazy dog')
-
-// const pangramFinder = new PangramFinder('this is not a pangram so the result should be false')
-
-//console.log(pangramFinder.isPangram())
