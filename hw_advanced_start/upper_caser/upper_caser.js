1a43d2c66d520405a21d10695656d4a38b6396cf
Alex <alexgjohn@outlook.com>
2023-05-12 16:20:40 +0100

    passed tests for upper_caser

--- hw_advanced_start/upper_caser/upper_caser.js
+++ hw_advanced_start/upper_caser/upper_caser.js
@@ -1,8 +1,25 @@
 const UpperCaser = function (words) {
+    this.words = words
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
+    const wordsInUpper = this.words.map((word) => {
+        return word.toUpperCase()
+    })
+    return wordsInUpper
 }
 
+// Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+//     const listOfDistances = this.journeys.map((journey) => {
+//       return journey.distance
+//     })
+  
+
 module.exports = UpperCaser;
+
+const upper = new UpperCaser(['hello'])
+
+console.log(upper.toUpperCase())
+
+
